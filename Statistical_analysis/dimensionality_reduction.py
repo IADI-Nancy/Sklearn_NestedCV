d04c19980eeb55d603a7fe999b3e1311a0e06ff2
TimZaragori <40388619+TimZaragori@users.noreply.github.com>
2020-04-03 16:19:10 +0200

    Update dimensionality_reduction.py
    Don't take callable as argument anymore. Take sklearn-like transformer instead

--- Statistical_analysis/dimensionality_reduction.py
+++ Statistical_analysis/dimensionality_reduction.py
@@ -3,18 +3,19 @@ import warnings
 import pandas as pd
 import numpy as np
 import sys
-from rpy2.robjects import r, pandas2ri
+from rpy2.robjects import r, pandas2ri, numpy2ri
 from Statistical_analysis.univariate_statistical_analysis import univariate_analysis
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.exceptions import NotFittedError
 pandas2ri.activate()
+numpy2ri.activate()
 
 
 class DimensionalityReduction(BaseEstimator):
     """A general class to handle dimensionality reduction.
         Parameters
         ----------
-        method: str or callable
+        method: str or transform
             Method used to compute dimensionality reduction. Either str or callable
             If str inbuild function named as str is called, must be one of following:
                 'hierarchical_clust_parmar': Consensus Clustering with hierarchical clustering as described in :
@@ -23,41 +24,16 @@ class DimensionalityReduction(BaseEstimator):
                 'hierarchical_clust_leger': Hierarchical clustering as described in :
                     A comparative study of machine learning methods for time-to-event survival data for
                     radiomics risk modelling. Leger et al., Scientific Reports, 2017
-            If callable method must take feature_array as arguments (and label_array if needed)
-            and return the coefficient matrix which is an array of shape (n_components, n_features)
-            with for each features the coefficient associated with each components of reduced features dataset
-        sklearn_kwargs: dict (default={})
-            Keyword arguments that will be used to create object if a sklearn class is used as method
-            Ex: method = sklearn.decomposition.PCA()
-                sklearn_kwargs={'n_components': 0.95, 'svd_solver': 'full'}
-        save_dir: str (default=None)
-            Path to the directory where optional clustering analysis could be saved as an excel file
-            If None, analysis won't be performed
+            If transform method must inherit from TransformMixin (like sklearn transformers) or have a fit + a transform
+            method that will be called successively with the latter returning the reduce dataset.
+            Ex :
+                str: method = 'hierarchical_clust_leger'
+                transform: method = sklearn.decomposition.PCA(n_components=0.95, solver='svd_full')
     """
     dr_methods = ['hierarchical_clust_parmar', 'hierarchical_clust_leger']
 
-    def __init__(self, method, sklearn_kwargs={}, save_dir=None):
-        self.sklearn_kwargs = sklearn_kwargs
-        if callable(method):
-            if not method.__name__ in self.dr_methods:
-                if not self.sklearn_kwargs:
-                    warnings.warn('WARNING : No keywords arguments were found.'
-                                  'The method object will be created with default parameter.')
-                self.method = method(**self.sklearn_kwargs)
-            else:
-                self.method = method
-        elif isinstance(method, str):
-            method = method.lower()
-            if method not in self.dr_methods:
-                raise ValueError('If string method must be one of : {0}. '
-                                 '%s was passed'.format(str(self.dr_methods), method))
-            self.method = getattr(self, method)
-        else:
-            raise TypeError('method argument must be a callable or a string')
-        self.save_dir = save_dir
-        if self.save_dir is not None:
-            if not os.path.exists(self.save_dir):
-                os.mkdir(self.save_dir)
+    def __init__(self, method='hierarchical_clust_leger'):
+        self.method = method
         self.is_reduced = False
         self.is_fitted = True
 
@@ -79,7 +55,7 @@ class DimensionalityReduction(BaseEstimator):
                 if isinstance(y, pd.DataFrame) or isinstance(y, pd.Series):
                     y = y.to_numpy()
                 else:
-                    raise TypeError('X array must be an array like or pandas Dataframe/Series')
+                    raise TypeError('y array must be an array like or pandas Dataframe/Series')
             else:
                 y = np.array(y)
             if len(y.shape) != 1:
@@ -91,26 +67,31 @@ class DimensionalityReduction(BaseEstimator):
                 raise ValueError('y array must have at least 2 classes')
         return X, y
 
+    def _get_dr_func(self):
+        if isinstance(self.method, TransformerMixin) or (hasattr(self.method, 'fit') and hasattr(self.method, 'transform')):
+            return self.method
+        elif isinstance(self.method, str):
+            method_name = self.method.lower()
+            if method_name not in self.dr_methods:
+                raise ValueError('If string method must be one of : {0}. '
+                                 '%s was passed'.format(str(self.dr_methods), self.method))
+            return getattr(self, self.method)
+        else:
+            raise TypeError('method argument must be a callable or a string')
+
     @staticmethod
-    def hierarchical_clust_parmar(X, save_dir, y=None):
+    def hierarchical_clust_parmar(X, y=None):
         """
         Consensus Clustering with hierarchical clustering as described in :
             Radiomic feature clusters and Prognostic Signatures specific for Lung and Head & Neck cancer.
             Parmar et al., Scientific Reports, 2015
-        save_dir must not be None to perform cluster analysis (cluster stability, cluster compactness) and get
-        ConsensusClusterPlus plots.
-        Cluster similarity between two different datasets and Cluster validation by comparison with test set need to be
-        calculated out of the pipeline.
         """
         df = pd.DataFrame(X)
         r_df = pandas2ri.py2ri(df)
         cwd = os.path.dirname(sys.argv[0])
         r.setwd(cwd)
         r.source('./Statistical_analysis/R_scripts/hierarchical_clustering_Parmar.R')
-        if save_dir:
-            r_dr_results = r.hierarchical_clustering_parmar(r_df, max_k=20, threshold=0.1, save_dir=save_dir)
-        else:
-            r_dr_results = r.hierarchical_clustering_parmar(r_df, max_k=20, threshold=0.1)
+        r_dr_results = r.hierarchical_clustering_parmar(r_df, max_k=20, threshold=0.1)
         R_object_dict = {}
         keys = r_dr_results.names
         for i in range(len(keys)):
@@ -124,24 +105,18 @@ class DimensionalityReduction(BaseEstimator):
         return coefficient_matrix
 
     @staticmethod
-    def hierarchical_clust_leger(X, save_dir, y=None):
+    def hierarchical_clust_leger(X, y=None):
         """
         Hierarchical clustering as described in :
             A comparative study of machine learning methods for time-to-event survival data for
             radiomics risk modelling. Leger et al., Scientific Reports, 2017
-        save_dir must not be None to perform cluster analysis (cluster compactness)
-        Cluster similarity between two different datasets and Cluster validation by comparison with test set need to be
-        calculated out of the pipeline.
         """
         df = pd.DataFrame(X)
         r_df = pandas2ri.py2ri(df)
         cwd = os.path.dirname(sys.argv[0])
         r.setwd(cwd)
         r.source('./Statistical_analysis/R_scripts/hierarchical_clustering_Leger.R')
-        if save_dir:
-            r_dr_results = r.hierarchical_clustering_leger(r_df, save_dir=save_dir)
-        else:
-            r_dr_results = r.hierarchical_clustering_leger(r_df)
+        r_dr_results = r.hierarchical_clustering_leger(r_df)
         R_object_dict = {}
         keys = r_dr_results.names
         for i in range(len(keys)):
@@ -183,12 +158,14 @@ class DimensionalityReduction(BaseEstimator):
                  each components of reduced features dataset
         """
         X, y = self._check_X_Y(X, y)
+        self.dr_func = self._get_dr_func()
+
         # Test whether the method has a fit function like sklearn classes
-        if hasattr(self.method, 'fit'):
-            self.method.fit(X, y)
+        if hasattr(self.dr_func, 'fit'):
+            self.dr_func.fit(X, y)
         else:
-            self.coefficient_matrix = self.method(X, self.save_dir, y)
-            self.is_fitted = True
+            self.coefficient_matrix = self.dr_func(X, y=y)
+        self.is_fitted = True
 
     def transform(self, X):
         """Reduce X dataset to create a new dataset.
@@ -202,26 +179,17 @@ class DimensionalityReduction(BaseEstimator):
             reduced_features
                  array of shape (n_samples, n_components) containing the reduced dataset
         """
-        # Check X
-        if not isinstance(X, (list, tuple, np.ndarray)):
-            if isinstance(X, pd.DataFrame) or isinstance(X, pd.Series):
-                X = X.to_numpy()
-            else:
-                raise TypeError('X array must be an array like or pandas Dataframe/Series')
-        else:
-            X = np.array(X)
-        if len(X.shape) != 2:
-            raise ValueError('X array must 2D')
+        X, _ = self._check_X_Y(X, None)
         # Test whether the method has a transform function like sklearn classes
-        if hasattr(self.method, 'transform'):
-            self.reduced_features = self.method.transform(X)
-        else:
-            if self.is_fitted:
-                self.reduced_features = np.array([self.coefficient_matrix.dot(X[_, :]) for _ in range(X.shape[0])])
-                self.is_reduced = True
-                return self.reduced_features
+        if self.is_fitted:
+            if hasattr(self.dr_func, 'transform'):
+                self.reduced_features = self.dr_func.transform(X)
             else:
-                raise NotFittedError('Fit method must be used before calling transform')
+                self.reduced_features = np.array([self.coefficient_matrix.dot(X[_, :]) for _ in range(X.shape[0])])
+            self.is_reduced = True
+            return self.reduced_features
+        else:
+            raise NotFittedError('Fit method must be used before calling transform')
 
     def fit_transform(self, X, y=None):
         """Fit dimensionality reduction and reduce X dataset to create a new dataset
@@ -241,9 +209,5 @@ class DimensionalityReduction(BaseEstimator):
                  array of shape (n_components, n_features) with for each features the coefficient associated with
                  each components of reduced features dataset
             """
-        # Test whether the method has a fit_transform function like sklearn classes
-        if hasattr(self.method, 'fit_transform'):
-            return self.method.fit_transform(X, y)
-        else:
-            self.coefficient_matrix = self.method(X, self.save_dir, y)
-            return self.transform(X)
+        self.fit(X, y)
+        return self.transform(X)
