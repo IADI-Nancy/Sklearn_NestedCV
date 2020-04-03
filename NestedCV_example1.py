82e09ef4582f672b804c9799eb5ab93f9e78612b
TimZaragori <40388619+TimZaragori@users.noreply.github.com>
2020-04-03 16:23:10 +0200

    Update NestedCV_example1.py

--- NestedCV_example1.py
+++ NestedCV_example1.py
@@ -1,43 +1,28 @@
-import imblearn
 import os
 import numpy as np
 import pandas as pd
 from Statistical_analysis.nested_cv import NestedCV
 from Statistical_analysis.univariate_statistical_analysis import univariate_analysis
-from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-from sklearn.datasets import load_breast_cancer
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
 
 
 def statistical_pipeline(X, y, save_dir=None, seed=111):
     # Univariate analysis
-    univariate_results = univariate_analysis(X, y, save_dir=save_dir)
-    if save_dir is not None:
-        save_path = os.path.join(save_dir, 'univariate_analysis.xlsx')
-        df = pd.DataFrame(univariate_results)
-        df.to_excel(save_path)
+    univariate_results = univariate_analysis(X, y, save_dir=None)
 
-    # NestedCV with outer loop and inner loop being 5Fold Stratified cross validation repeated 5 times
-    # Pipeline = Z-score normalization + SMOTE + Dimensionality reduction with Hierarchical clustering +
-    #            Feature selection with MW test score + SVM classifier
+    # NestedCV with outer loop and inner loop being 10Fold Stratified cross validation
+    # Pipeline = Z-score normalization + PCA + L2 Logistic Regression
+    pipeline_dic = {'scale': StandardScaler, 'DimensionalityReduction': PCA, 'classifier': LogisticRegression}
+    params_dic = {'DimensionalityReduction': {'n_components': [0.95, 0.99]},
+                  'classifier': {'C': 1 / np.arange(0.1, 10.1, 0.1)}}
+    pipeline_options = {'DimensionalityReduction': {'svd_solver': 'full', 'random_state': seed},
+                        'classifier': {'penalty': 'l2', 'random_state': seed, 'solver': 'saga', 'max_iter': 1e5}}
 
-    outer_cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=5, random_state=seed)
-    inner_cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=5, random_state=seed)
-    pipeline_dic = {'scale': StandardScaler,
-                    'oversampling': imblearn.over_sampling.SMOTE,
-                    'DimensionalityReduction': 'hierarchical_clust_leger',
-                    'FeatureSelection': 'mw',
-                    'classifier': SVC}
-    params_dic = {'classifier': {'C': 1 / np.arange(0.1, 1.1, 0.2)},
-                  'FeatureSelection': {'n_selected_features': [10, 20, None]}}
-    pipeline_options = {'oversampling': {'sampling_strategy': 'minority'},
-                        'FeatureSelection': {'bootstrap': True, 'ranking_aggregation': 'importance_score'},
-                        'classifier': {'kernel': 'linear', 'random_state': seed}}
-
-    clf = NestedCV(pipeline_dic, params_dic, outer_cv=outer_cv, inner_cv=inner_cv, n_jobs=-1,
-                   pipeline_options=pipeline_options,
-                   metric='roc_auc', verbose=2, refit=True, return_train_score=True, imblearn_pipeline=True)
+    clf = NestedCV(pipeline_dic, params_dic, outer_cv=10, inner_cv=10, n_jobs=-1, pipeline_options=pipeline_options,
+                   metric='roc_auc_ovo', verbose=2, refit=True, return_train_score=True, imblearn_pipeline=False)
     clf.fit(X, y)
 
     # Save outer results
@@ -48,7 +33,7 @@ def statistical_pipeline(X, y, save_dir=None, seed=111):
 
 
 # Load dataset
-breast = load_breast_cancer()
-X = breast.data
-y = breast.target
+iris = load_iris()
+X = iris.data
+y = iris.target
 statistical_pipeline(X, y)
