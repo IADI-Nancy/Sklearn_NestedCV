b0cc5a290926425bf4da534ae9e8e8e816c99048
TimZaragori <40388619+TimZaragori@users.noreply.github.com>
2020-04-04 18:56:44 +0200

    Update ComBat.R

--- Statistical_analysis/R_scripts/ComBat.R
+++ Statistical_analysis/R_scripts/ComBat.R
@@ -1,7 +1,7 @@
-ComBat_harmonization = function(data, covariate, batch, parametric, empirical_bayes, na_var = 'N/A')
+ComBat_harmonization = function(data, covariate, batch, parametric, empirical_bayes)
 {
   #=== Installing missing packages ===
-  package_list = c('stringr', 'matrixStats')
+  package_list = c('matrixStats')
   for (package in package_list)
   {
     if (!require(package, character.only = TRUE)){install.packages(package, repos = 'https://cloud.r-project.org/')}
@@ -10,14 +10,6 @@ ComBat_harmonization = function(data, covariate, batch, parametric, empirical_ba
   source("./R_scripts/scripts/utils.R")
   source("./R_scripts/scripts/combat.R")
   #=== Preprocess data ===
-  # Delete patients with N/A
-  NA_indices = which(apply(data, 1, function(x)str_detect(paste(x, collapse=""), paste(na_var, collapse="")))==TRUE)
-  if (length(NA_indices) != 0)
-  {
-    NA_rows <- data[NA_indices, ]
-    data <- data[-NA_indices, ]
-    covariate <- covariate[-NA_indices, ]
-  }
   data <- as.data.frame(sapply(data, as.numeric))
   data <- t(data)
   batch <- factor(x = batch, levels = unique(batch), labels = seq(length(unique(batch))))
@@ -25,10 +17,6 @@ ComBat_harmonization = function(data, covariate, batch, parametric, empirical_ba
   #=== ComBat ===
   data.harmonized <- combat(dat=data, batch=batch, parametric=parametric, mod=mod, eb=empirical_bayes)
   Combat_data <- as.data.frame(t(data.harmonized$dat.combat))
-  if (length(NA_indices) != 0)
-  {
-    Combat_data <- rbind(Combat_data, NA_rows)
-  }
   return(Combat_data)
 }
 
