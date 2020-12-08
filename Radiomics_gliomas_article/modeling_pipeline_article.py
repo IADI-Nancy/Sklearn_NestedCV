b31c96ba4e415044fbb8968be1bad56b9e0ceb8f
Timothée ZARAGORI <timothee.zaragori@univ-lorraine.fr>
2020-12-08 15:30:49 +0100

    estimate contribution of each set of features using model introspection with SHAP values instead of comparing model fitted with other datasets

--- Radiomics_gliomas_article/modeling_pipeline_article.py
+++ Radiomics_gliomas_article/modeling_pipeline_article.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import numpy as np
+import re
+import shap
+import matplotlib.pyplot as plt
 from Sklearn_NestedCV.master.Statistical_analysis.nested_cv import NestedCV
 from Sklearn_NestedCV.master.Statistical_analysis.data_harmonization import *
 from sklearn.preprocessing import StandardScaler
@@ -16,7 +19,8 @@ from sklearn.svm import SVC
 from itertools import combinations
 from sklearn.utils.fixes import loguniform
 from scipy.stats import randint
-from joblib import dump
+from joblib import dump, load
+from natsort import natsorted
 
 
 def save_results(save_dir, clf, X, y, score):
@@ -167,6 +171,34 @@ def save_results(save_dir, clf, X, y, score):
     # Extract predictions from model
     df = pd.DataFrame({key: clf.outer_pred[key] for key in clf.outer_pred if key != 'model'}, dtype='object')
     df.to_excel(os.path.join(save_dir, 'Model_predictions.xlsx'))
+
+
+def format_feature_name(feature_name):
+    del_substrings = ['original_']
+    if feature_name.endswith('TBR'):
+        del_substrings += ['TBR']
+    if 'ratio' in feature_name:
+        del_substrings += ['_ratio']
+    if 'merged' in feature_name:
+        del_substrings += ['_.3D._merged._']
+    else:
+        del_substrings += ['_.3D._']
+    for substring in del_substrings:
+        feature_name = feature_name.replace(substring, '')
+    if 'matrix' in feature_name:
+        matrix_name = feature_name.split('matrix')[0]
+        if 'occurrence' in matrix_name:
+            matrix_name = 'GLCM'
+        elif 'Run_length' in matrix_name:
+            matrix_name = 'GLRLM'
+        elif 'Size_zone' in matrix_name:
+            matrix_name = 'GLSZM'
+        elif 'Neighbourhood_grey_tone_difference' in matrix_name:
+            matrix_name = 'NGTDM'
+        elif 'Neighbouring_grey_level_dependence' in matrix_name:
+            matrix_name = 'NGLDM'
+        feature_name = '_'.join([matrix_name, feature_name.split('matrix')[1]])
+    return feature_name
 
 
 def get_models_randomsearch(output, algo_list, dr_list, fs_list):
@@ -346,7 +378,7 @@ if __name__ == '__main__':
                         default='./data/clinical_data.xlsx')
 
     parser.add_argument("--output", "--o", metavar='Output studied', help='Name of output studied',
-                        default='All')
+                        default='IDH')
 
     parser.add_argument("--threads", "--t", metavar='CPU jobs', help='Number of parallel threads',
                         default=-1, type=int)
@@ -399,15 +431,74 @@ if __name__ == '__main__':
         algo_name = '_'.join(model_name_splitted[-2:])
         fs = '_'.join(model_name_splitted[1:-2]).split('FS')[-1]
 
-    # === Fit best model on other datasets ===
-    feature_dic = get_features_dic(['S', 'S+D', 'S+OR'], dataset_dic, batch, harmonization, covariates)
-    models = get_models_randomsearch(output, [algo_name], [dim_red], [fs])
-    for feature_set in feature_dic:
-        print('\t\t\t================== %s %s %s ================' % (output, feature_set, best_model_name))
-        save_dir = os.path.join(os.path.abspath(args.save_root), args.root, output, feature_set, best_model_name)
-        os.makedirs(save_dir, exist_ok=True)
-        feature_dic[feature_set].to_csv(os.path.join(save_dir, '%s_harmonized_%s.csv' % (feature_set, harmonization)))
-        cv_results = statistical_pipeline(feature_dic[feature_set], output_df,
-                                          models[best_model_name]['pipeline_dic'],
-                                          models[best_model_name]['params_dic'], models[best_model_name]['pipeline_options'],
-                                          save_dir=save_dir, n_jobs=args.threads)
+    # === Get feature importance for best model ===
+    best_model_dir = os.path.join(os.path.abspath(args.save_root), args.root, output, 'S+D+OR', best_model_name)
+    pattern_feature = re.compile('FSwlcx_score_[a-zA-Z0-9]*_')
+    # Get mean number of features selected by the model
+    n_selected_metafeatures = re.findall(pattern_feature, best_model_name)[0][:-1].split('FS')[1].split('_')[-1]
+    if n_selected_metafeatures == 'None':
+        n_selected_metafeatures = None
+    else:
+        n_selected_metafeatures = int(n_selected_metafeatures)
+    if n_selected_metafeatures is None:
+        n_selected_features = n_selected_metafeatures
+    else:
+        pattern_fold = re.compile('outer[0-9]*f[0-9]*r')
+        n_splits, n_repeats = 10, 5
+        n_selected_features = []
+        selected_features_df = pd.read_excel(os.path.join(best_model_dir, 'Selected_features.xlsx'), index_col=0,
+                                             sheet_name=['outer%d' % (folds + 1) for folds in range(0, n_splits * n_repeats)])
+        for fold in selected_features_df:
+            metafeatures_selected = selected_features_df[fold][selected_features_df[fold]['Selected_features'] == True]
+            metafeatures_selected = metafeatures_selected.drop('Selected_features', axis='columns')
+            n_selected_features.append((metafeatures_selected != 0).sum(1).sum())
+        n_selected_features = int(np.round(np.mean(n_selected_features)))
+    pickle_file_list = natsorted([_ for _ in os.listdir(os.path.join(best_model_dir, 'Pickled_model')) if '.pkl' in _])
+    # Get feature names
+    df = pd.read_excel(os.path.join(best_model_dir, 'Selected_features.xlsx'), sheet_name='outer1', index_col=0)
+    feature_names = df.columns[:-1]
+    feature_names = [format_feature_name(_) for _ in feature_names]
+    df.columns = feature_names + [df.columns[-1]]
+    feature_importance_list = []
+    shap_values_list = []
+    for pickle_file in pickle_file_list:
+        model_dic = load(os.path.join(best_model_dir, 'Pickled_model', pickle_file))
+        outer_fold = 'outer%d' % (int(pickle_file.split('_')[-1].split('.')[0].split('outer')[-1]) + 1)
+        print('%s / %d' % (outer_fold, len(pickle_file_list)))
+        # Retrieve model information
+        model = model_dic['Model']
+        X_train, y_train, X_test, y_test = model_dic['X_train'], model_dic['y_train'], model_dic['X_test'], model_dic['y_test']
+        # Create model explainer
+        if isinstance(model[-1], RandomForestClassifier):
+            explainer = shap.TreeExplainer(model, feature_names=feature_names)
+        elif isinstance(model[-1], LogisticRegression):
+            explainer = shap.LinearExplainer(model, X_train, feature_names=feature_names)
+        else:
+            if hasattr(model[-1], 'decision_function'):
+                explainer = shap.KernelExplainer(model.decision_function, X_train)
+            else:
+                f = lambda x: model.predict_proba(x)[:, 1]
+                explainer = shap.KernelExplainer(f, X_train)
+        # Get SHAP values
+        shap_values = explainer.shap_values(X_test, n_samples=100)
+        shap_values_list.append(shap_values)
+    global_shap_values = np.concatenate(shap_values_list)
+    # Normalize shap so that sum is equal to 1
+    mean_shap = np.abs(global_shap_values).mean(0)
+    global_shap_values /= np.sum(mean_shap)
+    feature_importance = mean_shap / np.sum(mean_shap)
+    df = pd.DataFrame({'Feature': feature_names, 'Feature importance': feature_importance})
+    df.to_excel(os.path.join(best_model_dir, 'Global_feature_importance.xlsx'))
+    # Rank by importance
+    feature_order = np.argsort(feature_importance)
+    feature_order = feature_order[-n_selected_features:]
+
+    f = plt.figure()
+    shap.summary_plot(global_shap_values, feature_names=feature_names, plot_type='bar', show=False,
+                      max_display=n_selected_features)
+    ax = plt.gca()
+    ax.set_xlabel('Feature importance')
+    for index, value in enumerate(feature_importance[feature_order]):
+        ax.text(value, index, ' %1.1f%%' % (value * 100), va='center')
+    f.savefig(os.path.join(best_model_dir, 'Global_feature_importance.png'), bbox_inches='tight', dpi=600)
+    plt.close(f)
