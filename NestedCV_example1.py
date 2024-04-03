fb8f155e2c2525948f28b2cfa210fda232ce17e1
Timothée <zaragori.tim@gmail.com>
2024-04-03 11:50:30 +0200

    update examples

--- NestedCV_example1.py
+++ NestedCV_example1.py
@@ -1,8 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-from Statistical_analysis.nested_cv import NestedCV
-from Statistical_analysis.univariate_statistical_analysis import univariate_analysis
+from Statistical_analysis.nested_cv import GridSearchNestedCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
@@ -10,9 +9,6 @@ from sklearn.datasets import load_iris
 
 
 def statistical_pipeline(X, y, save_dir=None, seed=111):
-    # Univariate analysis
-    univariate_results = univariate_analysis(X, y, save_dir=None)
-
     # NestedCV with outer loop and inner loop being 10Fold Stratified cross validation
     # Pipeline = Z-score normalization + PCA + L2 Logistic Regression
     pipeline_dic = {'scale': StandardScaler, 'DimensionalityReduction': PCA, 'classifier': LogisticRegression}
@@ -21,13 +17,14 @@ def statistical_pipeline(X, y, save_dir=None, seed=111):
     pipeline_options = {'DimensionalityReduction': {'svd_solver': 'full', 'random_state': seed},
                         'classifier': {'penalty': 'l2', 'random_state': seed, 'solver': 'saga', 'max_iter': 1e5}}
 
-    clf = NestedCV(pipeline_dic, params_dic, outer_cv=10, inner_cv=10, n_jobs=-1, pipeline_options=pipeline_options,
-                   metric='roc_auc_ovo', verbose=2, refit=True, return_train_score=True, imblearn_pipeline=False)
+    clf = GridSearchNestedCV(pipeline_dic, params_dic, outer_cv=10, inner_cv=10, n_jobs=-1,
+                             pipeline_options=pipeline_options, metric='roc_auc_ovo', verbose=2, refit_inner=True,
+                             return_train_score=True, refit_outer=False)
     clf.fit(X, y)
-
+    print(clf.best_estimator_, clf.best_score_)
     # Save outer results
     if save_dir is not None:
-        save_path = os.path.join(save_dir, 'NestedCV_restuls.xlsx')
+        save_path = os.path.join(save_dir, 'NestedCV_results.xlsx')
         df = pd.DataFrame(clf.outer_results)
         df.to_excel(save_path)
 
--- NestedCV_example2.py
+++ NestedCV_example2.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 import pandas as pd
 from Statistical_analysis.nested_cv import NestedCV
-from Statistical_analysis.univariate_statistical_analysis import univariate_analysis
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -11,13 +10,6 @@ from sklearn.datasets import load_breast_cancer
 
 
 def statistical_pipeline(X, y, save_dir=None, seed=111):
-    # Univariate analysis
-    univariate_results = univariate_analysis(X, y, save_dir=save_dir)
-    if save_dir is not None:
-        save_path = os.path.join(save_dir, 'univariate_analysis.xlsx')
-        df = pd.DataFrame(univariate_results)
-        df.to_excel(save_path)
-
     # NestedCV with outer loop and inner loop being 5Fold Stratified cross validation repeated 5 times
     # Pipeline = Z-score normalization + SMOTE + Dimensionality reduction with Hierarchical clustering +
     #            Feature selection with Wilcoxon score + SVM classifier
@@ -37,7 +29,7 @@ def statistical_pipeline(X, y, save_dir=None, seed=111):
 
     clf = NestedCV(pipeline_dic, params_dic, outer_cv=outer_cv, inner_cv=inner_cv, n_jobs=-1,
                    pipeline_options=pipeline_options,
-                   metric='roc_auc', verbose=2, refit=True, return_train_score=True, imblearn_pipeline=True)
+                   metric='roc_auc', verbose=2, refit_inner=True, return_train_score=True, imblearn_pipeline=True)
     clf.fit(X, y)
 
     # Save outer results
